6d7056e353f07947a7ae7057404cf90c8d665d75
Vineet Raj <112626546+MrVineetRaj@users.noreply.github.com>
2024-07-03 01:44:38 +0530

    Update page.tsx

--- tts/src/app/page.tsx
+++ tts/src/app/page.tsx
@@ -1,52 +1,41 @@
 "use client";
 // app/page.tsx
+// pages/app/page.tsx
+
 import { useEffect, useState } from "react";
 
 export default function Home() {
   const text =
     "भगवद् गीता जीवन की रहस्योद्घाटन करने वाला, कर्म, ज्ञान और भक्ति के मार्गों का मार्गदर्शन करने वाला एक पवित्र ग्रंथ है।";
-  const handleSpeak = (voice: string = "Macedonian Male") => {
-    if (typeof responsiveVoice !== "undefined") {
-      responsiveVoice.speak(text, voice);
-    } else {
-      console.error("responsiveVoice is not defined");
+
+  const generateAudio = async () => {
+    try {
+      const response = await fetch("/api/stt", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ text }),
+      });
+
+      if (response.ok) {
+        console.log("Audio file generated successfully");
+        const data = await response.json();
+        console.log("Audio file URL:", data.url);
+      } else {
+        console.error("Failed to generate audio file");
+      }
+    } catch (error) {
+      console.error("Error generating audio file:", error);
     }
   };
 
-  useEffect(() => {
-    const checkResponsiveVoice = () => {
-      if (typeof responsiveVoice === "undefined") {
-        console.error("responsiveVoice is not loaded");
-      } else {
-        console.log("responsiveVoice is loaded");
-      }
-    };
-
-    checkResponsiveVoice();
-
-    // Optionally, add a small delay to ensure script loading
-    const interval = setInterval(() => {
-      if (typeof responsiveVoice !== "undefined") {
-        clearInterval(interval);
-        console.log("responsiveVoice is loaded");
-      }
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
-
   return (
     <div>
-      <h1>ResponsiveVoice with Next.js</h1>
+      <h1>Generate and Save Audio in Next.js</h1>
 
       <br />
-      <button
-        onClick={() => {
-          handleSpeak("Hindi Female");
-        }}
-      >
-        Speak
-      </button>
+      <button onClick={generateAudio}>Generate Audio</button>
     </div>
   );
 }
